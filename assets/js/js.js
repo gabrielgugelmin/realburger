e40f9ad3d3fb4ec21966c7cc049e12b24f7eb6d2
gabrielgugelmin <gaab.fg@gmail.com>
2017-09-27 22:47:11 -0300

    Início da parte mobile - Header e Galeria

---
+++ assets/js/js.js
@@ -0,0 +1,145 @@
+$(function(){
+  clickOutsideMenu();
+
+  // MENU
+  // click no hamburguer icon
+  $('.MenuTrigger').on('click', function(e){
+    e.preventDefault();   
+
+    if( $('.Menu').hasClass('Menu--open') ){
+      closeMenu();
+    } else{
+      openMenu();
+    }
+  });
+
+  // abre um sub menu
+  $('.Menu--hasSub > a').on('click', function(e){
+    e.preventDefault();
+
+    $(this).siblings('.Menu-sub').addClass('Menu-sub--subOpen');
+    $(this).addClass('is-selected');
+    $('.Menu').addClass('Menu--subOpen');
+    $('#Header').addClass('is-expanded');
+  });
+
+  // volta ao menu principal
+  $('.js-back').on('click', function(){
+    $(this).parent().removeClass('Menu-sub--subOpen');
+    $('.Menu--hasSub > a').removeClass('is-selected');
+    $('.Menu').removeClass('Menu--subOpen');
+    $('#Header').removeClass('is-expanded');
+  });
+
+  // menu fixo ao scollar
+  $(window).scroll(function(){
+    
+    if( $(this).scrollTop() >= 0 ){
+      $('#Header').addClass('is-scrolling');
+    } else{
+    	$('#Header').removeClass('is-scrolling');
+    }
+  });
+
+  // slider
+  $('.js-gallerySlider').slick({
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    speed: 800,
+    asNavFor: '.js-gallerySliderNav',
+    infinite: false,
+    mobileFirst: true
+  });
+
+  $('.js-gallerySliderNav').slick({
+    arrows: true,
+    asNavFor: '.js-gallerySlider',
+    centerMode: true,
+    focusOnSelect: true,
+    infinite: true,
+    mobileFirst: true,
+    nextArrow: '<button type="button" class="Arrow__button Arrow__button--next"></button>',
+    prevArrow: '<button type="button" class="Arrow__button Arrow__button--prev"></button>',
+    slidesToScroll: 2,
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          centerMode: true,
+          vertical: true,
+          verticalSwiping: true
+        }
+      }
+    ]
+  });
+  
+});
+
+function closeMenu(){
+  $('.Menu').removeClass('Menu--open');
+  $('.Menu').removeClass('Menu--subOpen');
+  $('#Header').removeClass('is-expanded');
+
+  $('.Menu-sub').removeClass('Menu-sub--subOpen');
+  $('.MenuTrigger').removeClass('is-open');
+
+  $('.Menu--hasSub > a').removeClass('is-selected');
+
+  $('body').removeClass('overflowHidden');
+}
+
+function openMenu(){
+  $('.MenuTrigger').addClass('is-open');
+  $('.Menu').addClass('Menu--open');
+  $('body').addClass('overflowHidden');
+}
+
+function viewport() {
+  var e = window, a = 'inner';
+  if (!('innerWidth' in window )) {
+      a = 'client';
+      e = document.documentElement || document.body;
+  }
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+}
+
+function checkWindowWidth(){
+  var w = viewport().width;
+  var size = '';
+  if(w > 991){
+    size = 'desktop';
+  } else{
+    size = 'mobile';
+  }
+
+  return size;
+}
+
+function clickOutsideMenu(){
+  
+  var x = checkWindowWidth();
+  if(x == 'desktop'){
+    $(document).on('mouseup', function (e){ 
+      var elem = $('.Menu-sub');
+
+      if (!elem.is(e.target) && elem.has(e.target).length === 0){
+        closeMenu();
+      }
+    });
+  } else{
+    $(document).off('mouseup');
+  }
+}
